45a5c8876da684a4b16a291ba77ac99c5adfbcbc
Evgrafov Denis <stereodenis@gmail.com>
2020-07-13 17:44:55 +0300

    Фото Избирательного округа

---
+++ src/images/areas/index.ts
@@ -0,0 +1,53 @@
+import area1 from './1.png'
+import area2 from './2.png'
+import area3 from './3.png'
+import area4 from './4.png'
+import area5 from './5.png'
+import area6 from './6.png'
+import area7 from './7.png'
+import area8 from './8.png'
+import area9 from './9.png'
+import area10 from './10.png'
+import area11 from './11.png'
+import area12 from './12.png'
+import area13 from './13.png'
+import area14 from './14.png'
+import area15 from './15.png'
+import area16 from './16.png'
+import area17 from './17.png'
+import area18 from './18.png'
+import area19 from './19.png'
+import area20 from './20.png'
+import area21 from './21.png'
+import area22 from './22.png'
+import area23 from './23.png'
+import area24 from './24.png'
+
+const areasImages: { [s: string]: string } = {
+  1: area1,
+  2: area2,
+  3: area3,
+  4: area4,
+  5: area5,
+  6: area6,
+  7: area7,
+  8: area8,
+  9: area9,
+  10: area10,
+  11: area11,
+  12: area12,
+  13: area13,
+  14: area14,
+  15: area15,
+  16: area16,
+  17: area17,
+  18: area18,
+  19: area19,
+  20: area20,
+  21: area21,
+  22: area22,
+  23: area23,
+  24: area24,
+}
+
+export default areasImages
--- src/screens/area/index.tsx
+++ src/screens/area/index.tsx
@@ -3,11 +3,13 @@ import { Container } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 // @ts-ignore
 import MetaTags from 'react-meta-tags'
+import { Image } from 'react-bootstrap'
 
 import candidats from '../../data/candidats'
 import deputats from '../../data/deputats'
 import { Deputat, Candidat } from '../../components'
 import areas from '../../data/areas'
+import areasImages from '../../images/areas'
 
 export default memo(() => {
   const { areaNumber } = useParams()
@@ -24,11 +26,15 @@ export default memo(() => {
           content={`Вся информация о кандидатах в депутаты избирательного округа №${areaNumber}`}
         />
         <meta property='og:title' content={`Избирательный округ ${areaNumber}`} />
-        <meta property='og:image' content={`../../images/areas/${areaNumber}.jpg`} />
+        <meta property='og:image' content={areasImages[areaNumber]} />
       </MetaTags>
       <h1 className='border-bottom pb-2'>{areaNumber} округ</h1>
 
       <div className='border-bottom pb-3'>
+        <Image src={areasImages[areaNumber]} />
+      </div>
+
+      <div className='border-bottom py-3'>
         <h2>Список кандидатов в депутаты</h2>
         <div className='d-flex flex-wrap flex-row'>
           {areaCandidats.map((candidat) => (
