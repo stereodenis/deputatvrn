d1d6244ff169e34ea6785d8ece136074b3ba62d3
Evgrafov Denis <stereodenis@gmail.com>
2020-07-27 12:23:42 +0300

    Логотипы партий

--- src/screens/partiesScreen/index.tsx
+++ src/screens/partiesScreen/index.tsx
@@ -1,10 +1,18 @@
 import React, { memo } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { shuffle } from 'lodash'
 
 import candidats from '../../data/candidats'
 import { Parties } from '../../types'
+
+const mapPartyToLogo: { [s: string]: string } = {
+  er: require('../../images/parties/er.png'),
+  kprf: require('../../images/parties/kprf.png'),
+  newPeople: require('../../images/parties/nl.jpg'),
+  rodina: require('../../images/parties/rodina.svg'),
+  yabloko: require('../../images/parties/yabloko.jpg'),
+}
 
 export default memo(() => {
   const noPartyCandidates = candidats.filter((c) => !c.party)
@@ -15,6 +23,12 @@ export default memo(() => {
       <Row>
         <Col xs={6} sm={4} md={3} lg={2} className='border py-3'>
           <Link to={'/parties/noParty'}>
+            <Image
+              width={'100%'}
+              className=''
+              src={require('../../images/parties/noparty.jpg')}
+              alt={'Самовыдвиженцы'}
+            />
             <div className='text-center'>Самовыдвиженцы</div>
             <div>Кандидатов: {noPartyCandidates.length}</div>
           </Link>
@@ -30,6 +44,12 @@ export default memo(() => {
           return (
             <Col xs={6} sm={4} md={3} lg={2} key={partyAlias} className='border py-3'>
               <Link to={`/parties/${partyAlias}`}>
+                <Image
+                  width={'100%'}
+                  className=''
+                  src={mapPartyToLogo[partyAlias]}
+                  alt={`Логотип ${Parties[partyAlias]}`}
+                />
                 <div className='text-center'>{Parties[partyAlias]}</div>
                 <div>Кандидатов: {partyCandidates.length}</div>
               </Link>
