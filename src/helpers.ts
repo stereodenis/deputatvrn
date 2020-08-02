9bce9d24de976106fbd6ba49db79838cc5f27cad
Evgrafov Denis <stereodenis@gmail.com>
2020-08-02 20:21:39 +0300

    Выводить текущих кандидатов

--- src/helpers.ts
+++ src/helpers.ts
@@ -6,7 +6,8 @@ import { Parties, Person } from './types'
  * Возвращает текущего кандидата для Person
  * @param person – объект Person
  */
-export const getCurrentCandidate = (person?: Person) => person?.candidate.find((c) => c.callNumber === CURRENT_CALL_NUMBER + 1)
+export const getCurrentCandidate = (person?: Person) =>
+  person?.candidate.find((c) => c.callNumber === CURRENT_CALL_NUMBER + 1)
 
 /**
  * Возвращает Person, которая является текущим депутатом в округе
@@ -14,7 +15,10 @@ export const getCurrentCandidate = (person?: Person) => person?.candidate.find((
  */
 export const getPersonWithCurrentDeputat = (areaNumber: number) =>
   persons.find((person) =>
-    person.candidate.find((candidate) => candidate.deputat && candidate.callNumber === CURRENT_CALL_NUMBER && candidate.areaNumber === areaNumber)
+    person.candidate.find(
+      (candidate) =>
+        candidate.deputat && candidate.callNumber === CURRENT_CALL_NUMBER && candidate.areaNumber === areaNumber
+    )
   )
 
 /* Кандидаты в депутаты нового созыва */
@@ -25,7 +29,9 @@ export const currentPersons = persons.filter(getCurrentCandidate)
  * @param areaNumber - номер избирательного округа
  */
 export const getAreaCandidates = (areaNumber: number) =>
-  currentPersons.filter((p) => p.candidate.find((c) => c.areaNumber === areaNumber))
+  currentPersons.filter((p) =>
+    p.candidate.find((c) => c.areaNumber === areaNumber && c.callNumber === CURRENT_CALL_NUMBER + 1)
+  )
 
 /**
  * Возвращает текущих кандидатов партии
