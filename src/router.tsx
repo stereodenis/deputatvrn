c39abd74d7bf902a3ef1201f6c3c35b722ad190b
Evgrafov Denis <stereodenis@gmail.com>
2020-08-04 14:32:26 +0300

    Редиректы старых урлов

--- src/router.tsx
+++ src/router.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 import { Main, Areas, Area, Candidates, Candidate, Parties, Party } from './screens'
 
@@ -19,6 +19,12 @@ import { Main, Areas, Area, Candidates, Candidate, Parties, Party } from './scre
 export default memo(() => (
   <Switch>
     <Route exact path='/' component={Main} />
+    <Redirect exact from={'/areas'} to={'/city/areas'} />
+    <Redirect from={'/areas/:areaNumber'} to={'/city/areas/:areaNumber'} />
+    <Redirect exact from={'/candidates'} to={'/city/candidates'} />
+    <Redirect from={'/candidates/:candidateAlias'} to={'/city/candidates/:candidateAlias'} />
+    <Redirect exact from='/parties' to={'/city/parties'} />
+    <Redirect from={'/parties/:partyAlias'} to={'/city/parties/:partyAlias'} />
     <Route exact path={'/:locationType/areas'} component={Areas} />
     <Route path={'/:locationType/areas/:areaNumber'} component={Area} />
     <Route exact path={'/:locationType/candidates'} component={Candidates} />
