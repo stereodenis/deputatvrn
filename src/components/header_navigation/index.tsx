89ed901994e19a0b5b602e49189bc0c62b90c980
Denis Evgrafov <stereodenis@gmail.com>
2020-08-18 21:15:55 +0300

    Yandex Donation (#112)

--- src/components/header_navigation/index.tsx
+++ src/components/header_navigation/index.tsx
@@ -49,6 +49,16 @@ export default memo(() => {
             <Nav.Link>Видео</Nav.Link>
           </LinkContainer>
           <Search />
+          <iframe
+            title='yandex-donation'
+            className='ml-1'
+            src='https://money.yandex.ru/quickpay/button-widget?targets=%D0%9D%D0%B0%20%D1%80%D0%B0%D0%B7%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D1%83%20%D1%81%D0%B0%D0%B9%D1%82%D0%B0%20%D0%BF%D1%80%D0%BE%20%D0%B4%D0%B5%D0%BF%D1%83%D1%82%D0%B0%D1%82%D0%BE%D0%B2%20%D0%92%D0%BE%D1%80%D0%BE%D0%BD%D0%B5%D0%B6%D0%B0&default-sum=100&button-text=14&yamoney-payment-type=on&button-size=m&button-color=orange&successURL=&quickpay=small&account=41001883464146&'
+            width='228'
+            height='36'
+            frameBorder='0'
+            allowTransparency
+            scrolling='no'
+          />
         </Nav>
       </Navbar.Collapse>
     </Navbar>
