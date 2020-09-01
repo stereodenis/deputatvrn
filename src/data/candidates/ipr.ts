150124242f533e231cac382de02eaa493f08cdf5
Evgrafov Denis <stereodenis@gmail.com>
2020-09-01 20:01:33 +0300

    списки ИПР

---
+++ src/data/candidates/ipr.ts
@@ -0,0 +1,726 @@
+import { LocationType, Person, Parties, CandidateStatuses } from '../../types'
+
+const ipr: Person[] = [
+  {
+    alias: 'getmanov_andrej',
+    name: 'Гетманов Андрей Владимирович',
+    birthdate: '17.05.1974',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 0,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'popov_mitrofan',
+    name: 'Попов Митрофан Федорович',
+    birthdate: '25.05.1946',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 0,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'ermolova_anna',
+    name: 'Ермолова Анна Викторовна',
+    birthdate: '02.11.1982',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 0,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'sencov_andrej',
+    name: 'Сенцов Андрей Анатольевич',
+    birthdate: '24.09.1988',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 1,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'vasileva_sofya',
+    name: 'Васильева Софья Алексеевна',
+    birthdate: '02.02.1992',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 1,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'voronov_vladislav',
+    name: 'Воронов Владислав Алексеевич',
+    birthdate: '07.10.1994',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 1,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'gorchakova_galina',
+    name: 'Горчакова Галина Владимировна',
+    birthdate: '30.10.1964',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 1,
+        listNumber: 4,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'morozov_vladimir',
+    name: 'Морозов Владимир Вячеславович',
+    birthdate: '07.12.1989',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 2,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'kislicyn_ivan',
+    name: 'Кислицын Иван Юрьевич',
+    birthdate: '09.11.1990',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 2,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'gavrilov_aleksandr',
+    name: 'Гаврилов Александр Андреевич',
+    birthdate: '21.08.1995',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 2,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'pustovalov_oleg',
+    name: 'Пустовалов Олег Валерьевич',
+    birthdate: '17.07.1992',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 3,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'medvedev_sergej',
+    name: 'Медведев Сергей Михайлович',
+    birthdate: '24.02.1991',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 3,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'zaharov_artyom',
+    name: 'Захаров Артём Сергеевич',
+    birthdate: '27.01.1996',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 3,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'zverev_pavel',
+    name: 'Зверев Павел Александрович',
+    birthdate: '13.04.1980',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 3,
+        listNumber: 4,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'artemev_aleksandr',
+    name: 'Артемьев Александр Геннадьевич',
+    birthdate: '28.07.1991',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 4,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'musin_vitalij',
+    name: 'Мусин Виталий Файрушевич',
+    birthdate: '17.05.1961',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 4,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'kamockij_nikita',
+    name: 'Камоцкий Никита Александрович',
+    birthdate: '24.04.1993',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 4,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'gorchakov_sergej',
+    name: 'Горчаков Сергей Вячеславович',
+    birthdate: '23.05.1986',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 4,
+        listNumber: 4,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'shchurov_aleksej',
+    name: 'Щуров Алексей Николаевич',
+    birthdate: '25.10.1977',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 5,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'usarov_ruslan',
+    name: 'Усаров Руслан Сохибжонович',
+    birthdate: '12.05.1989',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 5,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'kuzmin_aleksandr',
+    name: 'Кузьмин Александр Михайлович',
+    birthdate: '22.11.1996',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 5,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'rafailov_zhanuko',
+    name: 'Рафаилов Жануко Симонович',
+    birthdate: '20.10.1960',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 6,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'ulybasheva_zulejhat',
+    name: 'Улыбашева Зулейхат Александровна',
+    birthdate: '04.09.1959',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 6,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'rybchinskaya_polina',
+    name: 'Рыбчинская Полина Олеговна',
+    birthdate: '27.03.1988',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 6,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'emelyanov_anton',
+    name: 'Емельянов Антон Евгеньевич',
+    birthdate: '20.01.1987',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 7,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'kopejkin_vladimir',
+    name: 'Копейкин Владимир Сергеевич',
+    birthdate: '20.05.1976',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 7,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'serebryakova_kristina',
+    name: 'Серебрякова Кристина Игоревна',
+    birthdate: '16.03.1998',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 7,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'bahtina_galina',
+    name: 'Бахтина Галина Ивановна',
+    birthdate: '08.05.1952',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 8,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'daudov_musa',
+    name: 'Даудов Муса Абдулвахидович',
+    birthdate: '25.05.1955',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 8,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'timofeev_anton',
+    name: 'Тимофеев Антон Вячеславович',
+    birthdate: '02.03.1996',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 8,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'manukovskaya_valentina',
+    name: 'Мануковская Валентина Борисовна',
+    birthdate: '27.06.1955',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 9,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'popova_zoya',
+    name: 'Попова Зоя Семеновна',
+    birthdate: '05.08.1949',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 9,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'rybchinskaya_svetlana',
+    name: 'Рыбчинская Светлана Александровна',
+    birthdate: '23.05.1963',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 9,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'gudoshnikova_tatyana',
+    name: 'Гудошникова Татьяна Владимировна',
+    birthdate: '01.07.1961',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 10,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'gabibullaev_magamada',
+    name: 'Габибуллаев Магамада Магомедрамазанович',
+    birthdate: '13.09.1965',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 10,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'moskalev_mihail',
+    name: 'Москалев Михаил Юрьевич',
+    birthdate: '21.11.1990',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 10,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'starceva_lyudmila',
+    name: 'Старцева Людмила Сергеевна',
+    birthdate: '30.04.1984',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 11,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'pereverzev_valentin',
+    name: 'Переверзев Валентин Викторович',
+    birthdate: '13.03.1985',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 11,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'voronov_evgenij',
+    name: 'Воронов Евгений Владимирович',
+    birthdate: '03.09.1988',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 11,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'oboladze_inga',
+    name: 'Оболадзе Инга Георгиевна',
+    birthdate: '26.01.1966',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 12,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'bojko_elena',
+    name: 'Бойко Елена Анатольевна',
+    birthdate: '11.11.1980',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 12,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'barkalov_eduard',
+    name: 'Баркалов Эдуард Иванович',
+    birthdate: '02.06.1970',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 12,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'bajmatova_urmat',
+    name: 'Байматова Урмат Ажиматовна',
+    birthdate: '25.07.1964',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 22,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'alekseeva_kristina',
+    name: 'Алексеева Кристина Александровна',
+    birthdate: '24.03.1998',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 22,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'volohov_ivan',
+    name: 'Волохов Иван Владимирович',
+    birthdate: '16.01.1968',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 22,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'bachurin_vyacheslav',
+    name: 'Бачурин Вячеслав Владимирович',
+    birthdate: '06.11.1994',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 24,
+        listNumber: 1,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'fedorov_dmitrij',
+    name: 'Федоров Дмитрий Владимирович',
+    birthdate: '11.06.1995',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 24,
+        listNumber: 2,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+  {
+    alias: 'bogatkin_aleksandr',
+    name: 'Богаткин Александр Юрьевич',
+    birthdate: '01.02.1962',
+    candidate: [
+      {
+        locationType: LocationType.region,
+        callNumber: 7,
+        party: Parties.ipr,
+        areaNumber: 24,
+        listNumber: 3,
+        status: CandidateStatuses.declined,
+      },
+    ],
+  },
+]
+
+export default ipr
--- src/types.ts
+++ src/types.ts
@@ -69,6 +69,7 @@ export enum Parties {
   zaSprav = 'За справедливость!',
   dem = 'Демократическая партия России',
   kpkr = 'КПКР',
+  ipr = 'Интернациональная партия России',
 }
 
 export enum Project {
