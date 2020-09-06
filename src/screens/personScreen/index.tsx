5e77a65a4e4d4c2f8f6e2dd3db6048b0e2d34e66
Evgrafov Denis <stereodenis@gmail.com>
2020-09-06 22:42:54 +0300

    Сделать разделение по Город/Область на странице кандидата

--- src/screens/personScreen/index.tsx
+++ src/screens/personScreen/index.tsx
@@ -1,4 +1,4 @@
-import { flatten } from 'lodash'
+import { flatten, groupBy } from 'lodash'
 import moment from 'moment'
 import React, { memo, useMemo } from 'react'
 import { Col, Container, Image, Row } from 'react-bootstrap'
@@ -7,7 +7,7 @@ import { useParams, Link } from 'react-router-dom'
 import persons from '../../data/persons'
 import { isCurrentCallNumber, locationTypeFromName, youtube_parser, getCurrentCandidates } from '../../helpers'
 import { noPhoto } from '../../images/candidates'
-import { Candidate, LocationType } from '../../types'
+import { Candidate, LocationType, CandidateStatuses } from '../../types'
 import { CandidateStatus, PartyLink, AreaLink, CandidateCard } from '../../components'
 import videos from '../../data/videos'
 
@@ -32,6 +32,7 @@ export default memo(() => {
 
   const person = useMemo(() => persons.find((c) => c.alias === personAlias), [personAlias]) || persons[0]
   const personVideos = videos.filter((v) => v.objects.some((obj) => obj.type === 'person' && obj.id === person.alias))
+  const groupped = groupBy(person.candidate, (c) => c.locationType)
 
   return (
     <Container fluid>
@@ -61,90 +62,97 @@ export default memo(() => {
             )}
           </Row>
 
-          <Row>
-            {person.candidate.sort(sortCandidate).map((candidate) => {
-              const locationType = candidate.locationType === LocationType.city ? 'city' : 'region'
-              const currentPersons = persons.filter((p) => getCurrentCandidates(p, locationType).length > 0)
-              const areaPersonsAndCandidats = flatten(
-                currentPersons.map((p) =>
-                  getCurrentCandidates(p, locationType).map((c) => ({ person: p, candidate: c }))
-                )
-              ).filter((pc) => pc.candidate.areaNumber === candidate.areaNumber)
-
+          <Row className='flex-column flex-lg-row'>
+            {Object.keys(groupped).map((lt) => {
+              const locationCandidates = groupped[lt]
               return (
-                <div key={`${candidate.locationType}_${candidate.areaNumber}`} className='border-left pl-3 mb-3'>
-                  <div>
-                    {candidate.deputat && 'Депутат. '}
-                    {candidate.locationType}. {candidate.callNumber} созыв.{' '}
-                    <AreaLink areaNumber={candidate.areaNumber} locationName={candidate.locationType} />
-                    {'. '}
-                    <PartyLink party={candidate.party} locationName={candidate.locationType} />
-                    {'. '}
-                    {isCurrentCallNumber(candidate) && <CandidateStatus status={candidate.status} />}
-                  </div>
-                  {Boolean(candidate.program) && (
-                    <a href={candidate.program} className='d-block' rel='noopener noreferrer' target='_blank'>
-                      Программа
-                    </a>
-                  )}
-                  {Boolean(candidate.message) && (
-                    <div className='mt-3'>
-                      <p>«{candidate.message}»</p>
-                    </div>
-                  )}
-                  {/*{candidate.role && <div itemProp='description'>{candidate.role}</div>}*/}
-                  {/*{candidate.project && <div>{candidate.project}</div>}*/}
-                  {candidate.problems?.length && (
-                    <div className='mt-2'>
-                      <h5>
-                        Проблемы{' '}
-                        <Link to={`/${locationTypeFromName(candidate.locationType)}/areas/${candidate.areaNumber}`}>
-                          {candidate.areaNumber === 0 ? 'По общему списку' : `${candidate.areaNumber} округа`}
-                        </Link>
-                      </h5>
-                      {candidate.problems.map((problem, index) => (
-                        <p key={index}>{problem}</p>
-                      ))}
-                    </div>
-                  )}
+                <Col key={lt}>
+                  {locationCandidates.sort(sortCandidate).map((candidate) => {
+                    const locationType = candidate.locationType === LocationType.city ? 'city' : 'region'
+                    const currentPersons = persons.filter((p) => getCurrentCandidates(p, locationType).length > 0)
+                    const areaPersonsAndCandidats = flatten(
+                      currentPersons.map((p) =>
+                        getCurrentCandidates(p, locationType).map((c) => ({ person: p, candidate: c }))
+                      )
+                    )
+                      .filter((pc) => pc.candidate.areaNumber === candidate.areaNumber)
+                      .filter((pc) => pc.candidate.status === CandidateStatuses.registered)
+                      .filter((pc) => !pc.candidate.listNumber)
 
-                  {isCurrentCallNumber(candidate) && (
-                    <>
-                      <h2>
-                        Другие кандидаты{' '}
-                        <Link to={`/${locationTypeFromName(candidate.locationType)}/areas/${candidate.areaNumber}`}>
-                          {candidate.areaNumber === 0 ? 'По общему списку' : `${candidate.areaNumber} округа`}
-                        </Link>
-                      </h2>
-                      <Row>
-                        {areaPersonsAndCandidats
-                          .filter((pc) => pc.person.alias !== personAlias)
-                          .map((pc) => (
-                            <Col
-                              xs={6}
-                              md={4}
-                              lg={3}
-                              xl={2}
-                              key={pc.person.name}
-                              className='border-xs-top border-sm-none py-3'
-                            >
-                              <Link to={`/persons/${pc.person.alias}`}>
-                                <CandidateCard
-                                  {...{
-                                    name: pc.person.name,
-                                    photo: pc.person.photo,
-                                    candidate: pc.candidate,
-                                    locationType: locationTypeFromName(candidate.locationType),
-                                  }}
-                                  withParty
-                                />
+                    return (
+                      <div key={`${candidate.locationType}_${candidate.areaNumber}`} className='border p-1 mb-3'>
+                        <div>
+                          {candidate.deputat && 'Депутат. '}
+                          {candidate.locationType}. {candidate.callNumber} созыв.{' '}
+                          <AreaLink areaNumber={candidate.areaNumber} locationName={candidate.locationType} />
+                          {'. '}
+                          <PartyLink party={candidate.party} locationName={candidate.locationType} />
+                          {'. '}
+                          {isCurrentCallNumber(candidate) && <CandidateStatus status={candidate.status} />}
+                        </div>
+                        {Boolean(candidate.program) && (
+                          <a href={candidate.program} className='d-block' rel='noopener noreferrer' target='_blank'>
+                            Программа
+                          </a>
+                        )}
+                        {Boolean(candidate.message) && (
+                          <div className='mt-3'>
+                            <p>«{candidate.message}»</p>
+                          </div>
+                        )}
+                        {/*{candidate.role && <div itemProp='description'>{candidate.role}</div>}*/}
+                        {/*{candidate.project && <div>{candidate.project}</div>}*/}
+                        {candidate.problems?.length && (
+                          <div className='mt-2'>
+                            <h5>
+                              Проблемы{' '}
+                              <Link
+                                to={`/${locationTypeFromName(candidate.locationType)}/areas/${candidate.areaNumber}`}
+                              >
+                                {candidate.areaNumber === 0 ? 'По общему списку' : `${candidate.areaNumber} округа`}
                               </Link>
-                            </Col>
-                          ))}
-                      </Row>
-                    </>
-                  )}
-                </div>
+                            </h5>
+                            {candidate.problems.map((problem, index) => (
+                              <p key={index}>{problem}</p>
+                            ))}
+                          </div>
+                        )}
+
+                        {isCurrentCallNumber(candidate) && areaPersonsAndCandidats.length > 0 && (
+                          <>
+                            <h2>Другие кандидаты</h2>
+                            <Row>
+                              {areaPersonsAndCandidats
+                                .filter((pc) => pc.person.alias !== personAlias)
+                                .map((pc) => (
+                                  <Col
+                                    xs={6}
+                                    md={3}
+                                    lg={6}
+                                    xl={4}
+                                    key={pc.person.name}
+                                    className='border-xs-top border-sm-none py-3'
+                                  >
+                                    <Link to={`/persons/${pc.person.alias}`}>
+                                      <CandidateCard
+                                        {...{
+                                          name: pc.person.name,
+                                          photo: pc.person.photo,
+                                          candidate: pc.candidate,
+                                          locationType: locationTypeFromName(candidate.locationType),
+                                        }}
+                                        withParty
+                                      />
+                                    </Link>
+                                  </Col>
+                                ))}
+                            </Row>
+                          </>
+                        )}
+                      </div>
+                    )
+                  })}
+                </Col>
               )
             })}
           </Row>
